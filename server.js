3c0405df7db8e8a41602c9ef67e3d9db618e5cff
sagar singh <sagarsingh@sagars-MacBook-Pro.local>
2014-12-22 06:39:19 -0800

    patched 7ada0d0 by using the constructor properly

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 
-var _postList = []; // Local array of posts
+var _allPosts = [
+	new Post("I'm thankful for code 1"),
+	new Post("I'm thankful for code 2"),
+	new Post("I'm thankful for code 3")
+]; // Local array of posts
 
 function Post(text) {
 	this.text = text;
-	return this;
 }
