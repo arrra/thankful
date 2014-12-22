2bddb4598039bfbaa8e9d807c4bcf1f8f7d1e64d
sagar singh <sagarsingh@sagars-MacBook-Pro.local>
2014-12-22 06:51:01 -0800

    render all posts on the home page

--- server.js
+++ server.js
@@ -8,3 +8,25 @@ var _allPosts = [
 function Post(text) {
 	this.text = text;
 }
+
+var http = require('http'),
+    hogan = require('hogan.js'),
+       fs = require('fs');
+
+var server = http.createServer();
+server.on('request',function(req,res){
+
+	if(req.url == '/'){
+		fs.readFile('index.html',{encoding:'utf8'},function(err,contents){
+			if(err){
+				throw err; 
+				return ; 
+			}
+			var template = hogan.compile(contents);
+			var renderedHTML = template.render({posts: _allPosts});
+			res.setHeader('Content-Type','text/html');
+			res.end(renderedHTML);
+		});
+	}
+});
+server.listen(8080);
